61fa8b16d55a7c8c9af6b95d4df7154adbccc31b
CesarBerta <cesar_fb@hotmail.com>
2024-07-26 11:23:23 -0300

    add--partials

--- src/app.js
+++ src/app.js
@@ -19,6 +19,6 @@ const indexRouter = require('./routes/index.routes');
 app.use('/', indexRouter);
 
 app.use((req, res, next) => {
-    res.status(404).render('error404.ejs');
+    res.status(404).render('./partials/error404.ejs');
     next();
 });
